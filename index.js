262eae02db97786d06103d058775c8e174d6dfab
mryunt02 <mrthresh58@gmail.com>
2024-03-27 01:09:58 +0300

    web browser features

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function outer() {
-  debugger;
   let counter = 0;
   function incrementCounter() {
     counter++;
@@ -8,5 +7,16 @@ function outer() {
 }
 
 const myNewFunction = outer(); // we have access to the inner function incrementCounter = (closure)
-myNewFunction(); // logs 1
-myNewFunction(); // logs 2
+myNewFunction(); // counter=1
+myNewFunction(); // counter=2
+
+//Web Browser Features:
+// console();
+// fetch();
+// document;
+// setTimeout();
+// for instance
+setTimeout(() => {
+  console.log("Hello World");
+}, 1000);
+console.log("Me first!");
